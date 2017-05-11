80a4e225bf5f3c398950602d08d2b8d062667234
ZhangUP <ZhangUP@users.noreply.github.com>
2017-05-11 14:05:44 +0800

    Update SignatureOperationResult.java
    增加返回签字图像

--- src/com/unionpay/cloudpos/signature/SignatureOperationResult.java
+++ src/com/unionpay/cloudpos/signature/SignatureOperationResult.java
@@ -4,17 +4,24 @@ import com.unionpay.cloudpos.OperationResult;
 
 public interface SignatureOperationResult extends OperationResult {
     /**
-     * 返回签字
+     * 作电子签购单签名用途时，返回签字图像经JBIG1压缩后的数据
      * 
      * @return 签字buffer
      */
     byte[] getSignature();
     
     /**
-     * 返回签字长度
+     * 作电子签购单签名用途时，返回压缩数据长度
      * 
-     * @return 签字buffer长度
+     * @return buffer长度
      */    
     int getSignatureLength();
+    
+    /**
+     * 返回用户签字图像，如果是电子签购单，则返回用户签名和水印合成的图像；如果是其它签名，则直接返回用户签名图像
+     * 
+     * @return 签字图像
+     */    
+    Bitmap getSignature();
 
 }
